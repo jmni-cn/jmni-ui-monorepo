c18e8b460b41bcc93ed4c9eeb6eb1a5194f285e7
BILIBILI\v_zhangjiaming <https://git.bilibili.co/>
2023-01-18 11:32:00 +0800

    feat: 6单元测试 (二)： 搭建Vitest的单元测试环境

---
+++ src/button/__tests__/Button.spec.ts
@@ -0,0 +1,222 @@
+import Button from "../Button";
+
+import { shallowMount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
+// 测试分组
+describe('Button', () => {
+  // mount
+  test("mount  @vue/test-utils", () => {
+    // @vue/test-utils
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: 'Button'
+      }
+    });
+
+    // 断言
+    expect(wrapper.text()).toMatchInlineSnapshot('"Button"');
+
+  });
+
+  describe('color', () => {
+    test("default", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        }
+      });
+      expect(wrapper.classes().map(v => v.replace('\n','')).includes('bg-blue-500')).toMatchInlineSnapshot('true')
+    });
+
+    test("red", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: 'Button'
+        },
+        props: {
+          color: 'red'
+        }
+      });
+
+      expect(wrapper.classes().map(v => v.replace('\n',''))).toMatchInlineSnapshot(`
+        [
+          "",
+          "",
+          "py-1.5",
+          "px-3",
+          "rounded-lg",
+          "bg-red-500",
+          "hover:bg-red-400",
+          "border-red-500",
+          "cursor-pointer",
+          "border-solid",
+          "text-white",
+          "text-base",
+          "hover:text-white",
+          "transition",
+          "duration-300",
+          "ease-in-out",
+          "transform",
+          "hover:scale-105",
+          "mx-1",
+        ]
+      `)
+    });
+  })
+
+  describe("round", () => {
+    test("default", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: "Button",
+        },
+      });
+
+      expect(wrapper.classes().map(v => v.replace('\n',''))).toMatchInlineSnapshot(`
+        [
+          "",
+          "",
+          "py-1.5",
+          "px-3",
+          "rounded-lg",
+          "bg-blue-500",
+          "hover:bg-blue-400",
+          "border-blue-500",
+          "cursor-pointer",
+          "border-solid",
+          "text-white",
+          "text-base",
+          "hover:text-white",
+          "transition",
+          "duration-300",
+          "ease-in-out",
+          "transform",
+          "hover:scale-105",
+          "mx-1",
+        ]
+      `);
+    });
+
+    test("round:true", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: "Button",
+        },
+        props: {
+          round: true,
+        },
+      });
+
+      expect(wrapper.classes().map(v => v.replace('\n',''))).toMatchInlineSnapshot(`
+        [
+          "",
+          "",
+          "py-1.5",
+          "px-3",
+          "rounded-full",
+          "bg-blue-500",
+          "hover:bg-blue-400",
+          "border-blue-500",
+          "cursor-pointer",
+          "border-solid",
+          "text-white",
+          "text-base",
+          "hover:text-white",
+          "transition",
+          "duration-300",
+          "ease-in-out",
+          "transform",
+          "hover:scale-105",
+          "mx-1",
+        ]
+      `);
+    });
+  });
+
+  describe("plain", () => {
+    test("default", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: "Button",
+        },
+      });
+
+      expect(wrapper.classes().map(v => v.replace('\n',''))).toMatchInlineSnapshot(`
+        [
+          "",
+          "",
+          "py-1.5",
+          "px-3",
+          "rounded-lg",
+          "bg-blue-500",
+          "hover:bg-blue-400",
+          "border-blue-500",
+          "cursor-pointer",
+          "border-solid",
+          "text-white",
+          "text-base",
+          "hover:text-white",
+          "transition",
+          "duration-300",
+          "ease-in-out",
+          "transform",
+          "hover:scale-105",
+          "mx-1",
+        ]
+      `);
+    });
+
+    test("plain:true", () => {
+      const wrapper = shallowMount(Button, {
+        slots: {
+          default: "Button",
+        },
+        props: {
+          plain: true,
+        },
+      });
+
+      expect(wrapper.classes().map(v => v.replace('\n',''))).toMatchInlineSnapshot(`
+        [
+          "",
+          "",
+          "py-1.5",
+          "px-3",
+          "rounded-lg",
+          "bg-blue-100",
+          "hover:bg-blue-400",
+          "border-blue-500",
+          "cursor-pointer",
+          "border-solid",
+          "text-blue-500",
+          "text-base",
+          "hover:text-white",
+          "transition",
+          "duration-300",
+          "ease-in-out",
+          "transform",
+          "hover:scale-105",
+          "mx-1",
+        ]
+      `);
+    });
+  });
+
+  test("icon", () => {
+    const wrapper = shallowMount(Button, {
+      slots: {
+        default: "Button",
+      },
+      props: {
+        icon: "edit",
+      },
+    });
+
+    expect(wrapper.find("i").classes().map(v => v.replace('\n',''))).toMatchInlineSnapshot(`
+      [
+        "i-ic-baseline-edit",
+        "p-3",
+      ]
+    `);
+  });
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { presetUno, presetAttributify, presetIcons } from "unocss";
-// import Unocss from "unocss/vite";
 import Unocss from "./config/unocss";
 // https://vitejs.dev/config/
 const rollupOptions = {
@@ -20,22 +19,30 @@ export default defineConfig({
         vueJsx({
             // options are passed on to @vue/babel-plugin-jsx
         }),
-        // Unocss({
-        //     presets: [presetUno(), presetAttributify(), presetIcons()],
-        // })
         Unocss()
     ],
+    test: {
+        // enable jest-like global test APIs
+        globals: true,
+        // simulate DOM with happy-dom
+        // (requires installing happy-dom as a peer dependency)
+        environment: 'happy-dom',
+        // 支持tsx组件，很关键
+        transformMode: {
+            web: [/.[tj]sx$/]
+        }
+    },
     build: {
         rollupOptions,
-        minify:false,
+        minify: false,
         lib: {
-          entry: "./src/entry.ts",
-          name: "SmartyUI",
-          fileName: "smarty-ui",
-          // 导出模块格式
-          formats: ["es", "umd","iife"],
+            entry: "./src/entry.ts",
+            name: "SmartyUI",
+            fileName: "smarty-ui",
+            // 导出模块格式
+            formats: ["es", "umd", "iife"],
         },
         cssCodeSplit: true,
-      },
+    },
 
 });
